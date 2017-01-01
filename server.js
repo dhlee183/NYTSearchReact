e0de39ae6adc5563ba31452378e2787b71907e2c
David Hyun Lee <hyun183@Davids-MacBook-Pro.local>
2017-01-01 12:42:37 -0500

    Updated the code

--- server.js
+++ server.js
@@ -39,6 +39,52 @@ db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
+// Routes
+// ======
+
+// Simple index route
+app.get("/", function(req, res) {
+  res.send(index.html);
+});
+
+// A GET request to scrape the nytimes website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("http://www.nytimes.com/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.date = $(this).children("a").date();
+      result.url = $(this).children("a").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
+});
 
 // Listen on port 3000
 app.listen(3000, function() {
