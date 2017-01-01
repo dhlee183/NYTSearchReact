4b51a92f3d49f413a40f12fd1787885625f6e9b5
David Hyun Lee <hyun183@Davids-MacBook-Pro.local>
2017-01-01 12:46:29 -0500

    Updated descriptions

--- server.js
+++ server.js
@@ -16,25 +16,21 @@ mongoose.Promise = Promise;
 // Initialize Express
 var app = express();
 
-// Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-// Make public a static dir
 app.use(express.static("public"));
 
 // Database configuration with mongoose
 mongoose.connect("mongodb://localhost/week18day3mongoose");
 var db = mongoose.connection;
 
-// Show any mongoose errors
 db.on("error", function(error) {
   console.log("Mongoose Error: ", error);
 });
 
-// Once logged in to the db through mongoose, log a success message
 db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
@@ -49,32 +45,22 @@ app.get("/", function(req, res) {
 
 // A GET request to scrape the nytimes website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with request
   request("http://www.nytimes.com/", function(error, response, html) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
-    // Now, we grab every h2 within an article tag, and do the following:
     $("article h2").each(function(i, element) {
 
-      // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this).children("a").text();
       result.date = $(this).children("a").date();
       result.url = $(this).children("a").attr("href");
 
-      // Using our Article model, create a new entry
-      // This effectively passes the result object to the entry (and the title and link)
       var entry = new Article(result);
 
-      // Save that entry to the db
       entry.save(function(err, doc) {
-        // Log any errors
         if (err) {
           console.log(err);
         }
-        // Or log the doc
         else {
           console.log(doc);
         }
@@ -82,7 +68,6 @@ app.get("/scrape", function(req, res) {
 
     });
   });
-  // Tell the browser that we finished scraping the text
   res.send("Scrape Complete");
 });
 
